bc819fe779a51a547cba4a9b479d91277148942e
Biktoz <77498458+biktoz@users.noreply.github.com>
2023-09-06 23:53:44 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pigeonet Status</title>
+    <link rel="shortcut icon" href="status/images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="status/style/index.css">
+</head>
+
+<body>
+    <h1 class="title">Servizi di Pigeonet</h1>
+    <div class="container">
+        <div class="demos">
+            <div class="demo-down">
+                <span class="server-status" type="down"></span>
+                <span>Registrazione</span>
+            </div>
+            <div class="demo-maintenance">
+                <span class="server-status" type="maintenance"></span>
+                <span>Gateway</span>
+            </div>
+            <div class="demo-maintenance">
+                <span class="server-status" type="maintenance"></span>
+                <span>Uploader</span>
+            </div>
+
+            <!-- <div class="demo-up">
+                <span class="server-status" type="up"></span>
+                <span>Registrazione</span>
+            </div> -->
+            <!-- <div class="demo-slow">
+                <span class="server-status" type="slow"></span>
+                <span>Server is slow to respond, could be caused by some DDoS.</span>
+            </div>
+            <div class="demo-down">
+                <span class="server-status" type="down"></span>
+                <span>Server is not responding, down.</span>
+            </div> -->
+        </div>
+    </div>
+
+    
+    <h1 class="title">Attività recenti:</h1>
+    <div class="act-container">
+        <div class="activities">
+            <div class="activity">
+                <h3>06/09/2023</h3>
+                <p>Inauguriamo la pagina dello status</p>
+            </div>
+        </div>
+    </div>
+<br><br><br><br>
+</body>
+
+</html>
